4d0d4dd3f9784b0042baac5dbe26411f72ba384f
Emmanuel_123 <semazaemmanuel@gmail.com>
2024-05-31 05:51:17 -0700

    new

--- adminConn.php
+++ adminConn.php
@@ -2,7 +2,7 @@
     $server = 'localhost';
     $user = 'root';
     $password = '';
-    $db = 'voting system';
+    $db = 'election_system';
 
     $conn = mysqli_connect($server, $user, $password, $db);
     
